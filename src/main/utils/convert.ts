9d5eb398d505626667252627cd1498ee1c6707d5
iclee <iclee@ibleaders.co.kr>
2026-01-09 14:11:00 +0900

    fix: 데이터 길이 처리 방식을 비트 연산으로 변경

--- src/main/utils/convert.ts
+++ src/main/utils/convert.ts
@@ -10,40 +10,27 @@ interface AsterixMetaInfo {
  * @param input 문자열
  * @returns 8비트 배열
  */
-const strToBitArr = (input: string): AsterixMetaInfo => {
+const strToBitArr = (input: string): AsterixMetaInfo => { 
     // 입력받는 16진수 문자열 길이
     const len = input.length;
 
-    // ! 8비트
-    if ((len % 2) != 0) {
+    // ! 비트 연산으로 홀수를 체크
+    if ((len & 1) !== 0) {
         throw new Error('데이터 구조 오류 – 데이터 길이 제약 위반');
     }
 
     // * 16진수 문자열을 바이트 배열로 변환
     const bitArr = new Uint8Array(len / 2);
-    // * 검증용 데이터 - 데이터 총 길이 
-    let lenInfo = '';
 
     for (let index = 0; index < len; (index += 2)) {
         // * 2자리씩 끊어서 처리
         const eachStr = input.substring(index, (index + 2));
-
-        // * 데이터 길이 부분
-        if (index === 2 || index === 4) {
-            lenInfo += eachStr;
-            // 해당 위치는 0으로 임시 저장
-            bitArr[index / 2] = 0;
-            continue;
-        }
-
-        // * 8비트로 변환
         const eachBit = parseInt(eachStr, 16);
 
-        // ! 16진수
         if (isNaN(eachBit)) {
             throw new Error('데이터 구조 오류 - Asterix 규격에 부합하지 않는 데이터');
         }
-
+        
         bitArr[index / 2] = eachBit;
     }
 
@@ -53,7 +40,7 @@ const strToBitArr = (input: string): AsterixMetaInfo => {
     }
 
     // ! 전체 데이터 길이
-    const validForLen = parseInt(lenInfo, 16);
+    const validForLen = (bitArr[1] << 8) | bitArr[2];
 
     if (validForLen != bitArr.length) {
         throw new Error('데이터 구조 오류 - 불완전한 데이터 감지')
